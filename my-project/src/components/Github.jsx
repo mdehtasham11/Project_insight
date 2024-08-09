7b6db2477f956af170f436097d91b8b285cbf4dd
mdehtasham11 <fostersam058@gmail.com>
2024-08-09 23:21:07 +0530

    loader

--- my-project/src/components/Github.jsx
+++ my-project/src/components/Github.jsx
@@ -1,23 +1,33 @@
 import React, { useEffect, useState } from 'react'
-
+import { useLoaderData } from 'react-router-dom'
 function Github() {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/mdehtasham11')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                setData(data)
-            })
-    }, [])
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/mdehtasham11')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data);
+    //             setData(data)
+    //         })
+    // }, [])
+    const data = useLoaderData();
+
+
     return (
         <>
-            <div className='text-gray-600 m-4 text-center bg-orange-500 text-3xl'>
+        <div className='flex items-start'>
+            <img src={data.avatar_url} alt="git pic" className='width' />
+            <div className='text-gray-600 m-4 text-center text-3xl flex '>
                 Github Followers: {data.followers}
             </div>
-            <img src={data.avatar_url} alt="git pic" className='width' />
+        </div>
         </>
     )
 }
 
 export default Github
+
+export const GithubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/mdehtasham11')
+    return response.json()
+}
--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -2,39 +2,57 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {
+  Route,
   createBrowserRouter,
   RouterProvider,
+  createRoutesFromElements
 } from "react-router-dom";
 import Layout from './Layout.jsx';
 import About from './components/About.jsx';
 import Home from './components/Home.jsx';
 import Contact from './components/Contact.jsx';
-import Github from './components/Github.jsx';
+import Github, {GithubInfoLoader}from './components/Github.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />
-      },
-      {
-        path: "about",
-        element: <About />
-      },
-      {
-        path: "contact",
-        element: <Contact />
-      },
-      {
-        path: "github",
-        element : <Github />
-      }
-    ]
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       },
+//       {
+//         loader: {GithubInfoLoader},
+//         path: "github",
+//         element : <Github />
+//       }
+//     ]
+//   },
+// ]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      {/* <Route path='user/:userid' element={<User />} /> */}
+      <Route 
+      loader={GithubInfoLoader}
+      path='github' 
+      element={<Github />}
+       />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
